0c6afc74e2d72fa0b3423b0083d4638977d7edea
patrickdykes203-dot <patrickdykes203@gmail.com>
2026-02-13 01:14:54 -0600

    Update graves.js

--- graves.js
+++ graves.js
@@ -6,14 +6,5 @@ const graves = [
     lat: 39.736500,
     lng: -93.636222,
     details: "Wife of J. W. Hall — Mother, Gone but not forgotten"
-  },
-  
-  {
-    name: "Delila Miller",
-    birth: "1801",
-    death: "Dec 1, 1868",
-    lat: 39.736500,
-    lng: -93.636160,
-    details: "Wife of Rev. Jacob Miller"
   }
 ];
