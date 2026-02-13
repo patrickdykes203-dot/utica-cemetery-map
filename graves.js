0211c180b33d4b907b728f3dad759d4ca9765792
patrickdykes203-dot <patrickdykes203@gmail.com>
2026-02-13 00:33:03 -0600

    Create graves.js

---
+++ graves.js
@@ -0,0 +1,10 @@
+const graves = [
+  {
+    name: "Catherine Hall",
+    birth: "Oct 14, 1849",
+    death: "Mar 3, 1871",
+    lat: 39.736500,
+    lng: -93.636222,
+    details: "Wife of J. W. Hall — Mother, Gone but not forgotten"
+  }
+];
