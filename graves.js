9284246848efa3fda4ed8896a08ac543b876d21f
patrickdykes203-dot <patrickdykes203@gmail.com>
2026-02-13 01:51:53 -0600

    Update graves.js

--- graves.js
+++ graves.js
@@ -1,18 +1,26 @@
 const graves = [
   {
-    name: "Catherine Hall",
-    birth: "Oct 14, 1849",
-    death: "Mar 3, 1871",
     lat: 39.736500,
     lng: -93.636222,
-    details: "Wife of J. W. Hall — Mother, Gone but not forgotten"
+    people: [
+      {
+        name: "Catherine Hall",
+        birth: "Oct 14, 1849",
+        death: "Mar 3, 1871",
+        details: "Wife of J. W. Hall — Mother, Gone but not forgotten"
+      }
+    ]
   },
   {
-    name: "Delila Miller",
-    birth: "March 28, 1800",
-    death: "January 1, 1868",
     lat: 39.736510,
     lng: -93.636150,
-    details: "Aged 67 years, 9 months, 4 days, Wife of Rev Jacob Miller"
+    people: [
+      {
+        name: "Delila Miller",
+        birth: "March 28, 1800",
+        death: "January 1, 1868",
+        details: "Aged 67 years, 9 months, 4 days"
+      }
+    ]
   }
 ];
